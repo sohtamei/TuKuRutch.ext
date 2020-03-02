3292f227b73e3cb2baf21789a3f59d9e29714a61
sohta02 <sohta02@fa2.so-net.ne.jp>
2020-03-02 12:52:41 +0900

    copy.bat追加

---
+++ libraries/copy.bat
@@ -0,0 +1,30 @@
+set DIRNAME_S=RemoconRobo
+
+set DIRNAME=Buzzer
+copy %DIRNAME_S%\robot.js %DIRNAME%\robot.js /Y
+copy %DIRNAME_S%\robot_pcmode\robot_pcmode.ino.template %DIRNAME%\robot_pcmode\robot_pcmode.ino.template /Y
+
+set DIRNAME=esp32
+copy %DIRNAME_S%\robot.js %DIRNAME%\robot.js /Y
+copy %DIRNAME_S%\robot_pcmode\robot_pcmode.ino.template %DIRNAME%\robot_pcmode\robot_pcmode.ino.template /Y
+
+set DIRNAME=LiquidCrystal
+copy %DIRNAME_S%\robot.js %DIRNAME%\robot.js /Y
+copy %DIRNAME_S%\robot_pcmode\robot_pcmode.ino.template %DIRNAME%\robot_pcmode\robot_pcmode.ino.template /Y
+
+set DIRNAME=m5stack
+copy %DIRNAME_S%\robot.js %DIRNAME%\robot.js /Y
+copy %DIRNAME_S%\robot_pcmode\robot_pcmode.ino.template %DIRNAME%\robot_pcmode\robot_pcmode.ino.template /Y
+
+set DIRNAME=mBot
+copy %DIRNAME_S%\robot.js %DIRNAME%\robot.js /Y
+copy %DIRNAME_S%\robot_pcmode\robot_pcmode.ino.template %DIRNAME%\robot_pcmode\robot_pcmode.ino.template /Y
+
+set DIRNAME=QuadCrawler
+copy %DIRNAME_S%\robot.js %DIRNAME%\robot.js /Y
+copy %DIRNAME_S%\robot_pcmode\robot_pcmode.ino.template %DIRNAME%\robot_pcmode\robot_pcmode.ino.template /Y
+
+set DIRNAME=Spresense
+copy %DIRNAME_S%\robot.js %DIRNAME%\robot.js /Y
+copy %DIRNAME_S%\robot_pcmode\robot_pcmode.ino.template %DIRNAME%\robot_pcmode\robot_pcmode.ino.template /Y
+
