45255873e6eebbab6cf8b6d42be4b24c34e297cf
sohta02 <sohta02@fa2.so-net.ne.jp>
2020-08-12 13:47:12 +0900

    - template削除、path変更

--- libraries/copyToScratch3.0.sh
+++ libraries/copyToScratch3.0.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-SC3_PATH="/mnt/c/Users/n-tom/scratch3"
+BASE_PATH="../../../"
 
 copy_sb3 () {
   if [ "$3" ]; then
-    cat $1/robot_pcmode/robot_pcmode.js | sed -e "s/DummyName/$2/g" | sed -e "s/const $3 = false;/const $3 = true;/" > $SC3_PATH/scratch-vm/src/extensions/scratch3_tukurutch/$2.js
+    cat $1/robot_pcmode/robot_pcmode.js | sed -e "s/DummyName/$2/g" | sed -e "s/const $3 = false;/const $3 = true;/" > $BASE_PATH/scratch-vm/src/extensions/scratch3_tukurutch/$2.js
   else
-    cat $1/robot_pcmode/robot_pcmode.js | sed -e "s/DummyName/$2/g" > $SC3_PATH/scratch-vm/src/extensions/scratch3_tukurutch/$2.js
+    cat $1/robot_pcmode/robot_pcmode.js | sed -e "s/DummyName/$2/g" > $BASE_PATH/scratch-vm/src/extensions/scratch3_tukurutch/$2.js
   fi
 }
 
