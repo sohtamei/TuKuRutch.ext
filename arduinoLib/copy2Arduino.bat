559438c6fc320b9ca6b282363ab37cebecdd396e
sohta02 <sohta02@fa2.so-net.ne.jp>
2020-12-28 03:05:48 +0900

    - M5TimerCam追加

--- arduinoLib/copy2Arduino.bat
+++ arduinoLib/copy2Arduino.bat
@@ -1,6 +1,6 @@
-rmdir                ..\..\Arduino\portable\sketchbook\libraries\analogRemote\  /S /Q
+rmdir                    ..\..\Arduino\portable\sketchbook\libraries\analogRemote\  /S /Q
 
-xcopy analogRemote   ..\..\Arduino\portable\sketchbook\libraries\analogRemote\  /S /Q /Y
-xcopy TukurutchEsp   ..\..\Arduino\portable\sketchbook\libraries\TukurutchEsp\  /S /Q /Y
-xcopy esp_camera     ..\..\Arduino\portable\sketchbook\libraries\esp_camera\    /S /Q /Y
+xcopy analogRemote       ..\..\Arduino\portable\sketchbook\libraries\analogRemote\  /S /Q /Y
+xcopy TukurutchEsp       ..\..\Arduino\portable\sketchbook\libraries\TukurutchEsp\  /S /Q /Y
+xcopy TukurutchEspCamera ..\..\Arduino\portable\sketchbook\libraries\TukurutchEspCamera\    /S /Q /Y
 pause
