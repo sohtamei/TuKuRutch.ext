5f7879b6eca5cc2b0c8158d1b0d3d354165b544a
sohtamei <sohta02@fa2.so-net.ne.jp>
2021-05-04 15:55:07 +0900

    - camera ON/OFF対応 - microbit BLE不具合修正

--- libraries/microbitV2/robot_pcmode/main.cpp
+++ libraries/microbitV2/robot_pcmode/main.cpp
@@ -8,8 +8,16 @@
 microbit_Screen SCREEN;
 
 // microbit V1.5
+#if 1
 #include <LSM303AGR_ACC_Sensor.h>
 LSM303AGR_ACC_Sensor AccNew(&Wire);
+#else
+#include <Adafruit_LSM303_Accel.h>
+#include <Adafruit_Sensor.h>
+
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_LSM303_Accel_Unified AccNew = Adafruit_LSM303_Accel_Unified(54321);
+#endif
 
 //nclude <LSM303AGR_MAG_Sensor.h>
 //LSM303AGR_MAG_Sensor Mag(&Wire);
@@ -35,8 +43,6 @@ MMA8653 AccOld;
 #define PIN_WIRE_SDA         (20)
 */
 
-static uint32_t curStatus = 0;
-static uint32_t lastStatus = 0;
 static uint32_t eventFlag = 0;
 enum {
 	EVENT_PORT0		= (1<<0),
@@ -88,11 +94,20 @@ void _setup(const char* ver)
 	ret = Wire.endTransmission();
 	if(!ret) {
 		boardType = BOARDTYPE_NEW;
+	#if 1
 		AccNew.begin();
 		AccNew.Enable();
 	//	AccNew.EnableTemperatureSensor();
 	//	Mag.begin();
 	//	Mag.Enable();
+	#else
+		if(AccNew.begin()) {
+			AccNew.setRange(LSM303_RANGE_2G);
+			AccNew.setMode(LSM303_MODE_NORMAL);
+		} else {
+			Serial.println("error");
+		}
+	#endif
 	} else {
 		Wire.beginTransmission(I2C_ACCEL_OLD);
 		ret = Wire.endTransmission();
@@ -106,19 +121,23 @@ void _setup(const char* ver)
 //	Serial.println(boardType);
 }
 
-static int16_t tiltX = 0;
-static int16_t tiltY = 0;
-static int32_t lastAccel[3] = {0};
-
 static void _getAccel(int32_t accel[3])
 {
 	switch(boardType) {
 	case BOARDTYPE_NEW:
+	#if 1
 		AccNew.GetAxes(accel);
 	//	float temp;				// C
 	//	AccNew.GetTemperature(&temp);
 	//	int32_t mag[3];			// mGauss
 	//	Mag.GetAxes(mag);
+	#else
+		sensors_event_t event;
+		AccNew.getEvent(&event);
+		accel[0] = event.acceleration.x;
+		accel[1] = event.acceleration.y;
+		accel[2] = event.acceleration.z;
+	#endif
 		break;
 	case BOARDTYPE_OLD:
 		AccOld.update();
@@ -129,36 +148,15 @@ static void _getAccel(int32_t accel[3])
 	}
 }
 
+static uint32_t lastPort = 0;
 void _loop(void)
 {
-	int i;
-	curStatus = 0;
-
 	// port0,1,2 : H->L
-	curStatus |= ((digitalRead(2)<<2)|(digitalRead(1)<<1)|(digitalRead(0)<<0)) ^ B111;
+	uint32_t curPort = ((digitalRead(2)<<2)|(digitalRead(1)<<1)|(digitalRead(0)<<0)) ^ B111;
+	eventFlag |= (lastPort ^ curPort) & curPort;
+	lastPort = curPort;
 
-	// accel
-	int32_t accel[3] = {0};		// mg
-	_getAccel(accel);
-	tiltX =  atan2(accel[0], -accel[2]) * (180.0 * 10.0 / PI);
-	tiltY = -atan2(accel[1], -accel[2]) * (180.0 * 10.0 / PI);
-
-	uint32_t sumAccel = 0;
-	for(i = 0; i < 3; i++)
-		sumAccel += abs(accel[i] - lastAccel[i]);
-	if(sumAccel >=  400) curStatus |= EVENT_MOVED;
-	if(sumAccel >= 2000) curStatus |= EVENT_SHAKEN;
-	memcpy(lastAccel, accel, sizeof(lastAccel));
-
-	eventFlag |= (lastStatus ^ curStatus) & curStatus;
-	lastStatus = curStatus;
-#if 0
-char buf[100] = {0};
-int ret = snprintf(buf,64,"%5ld %5ld %5ld %4ld %5ld %5ld ", accel[0], accel[1], accel[2], tiltX/10, tiltY/10, sumAccel);
-for(i=0;i<12;i++) buf[ret+i] = (curStatus & (1<<(11-i))) ? '1': '0';
-Serial.println(buf);
-#endif
-
+	int i;
 	for(i = 0; i < 10; i++) {
 		SCREEN.loopScreen();
 		delay(1);
@@ -193,19 +191,37 @@ int _getTilt(uint8_t xy)
 		return  atan2(accel[0], -accel[2]) * (180.0 * 10.0 / PI);	// tiltX
 }
 
+static int32_t lastAccel[3] = {0};
 void _getSendData(void)
 {
-	uint8_t buf[8]={0};
+	// accel
+	int32_t accel[3] = {0};		// mg
+	_getAccel(accel);
+	int16_t tiltX =  atan2(accel[0], -accel[2]) * (180.0 * 10.0 / PI);
+	int16_t tiltY = -atan2(accel[1], -accel[2]) * (180.0 * 10.0 / PI);
 
-	uint32_t evt = eventFlag; eventFlag = 0;
-	buf[0] = evt;
-	buf[1] = evt>>8;
+	uint32_t sumAccel = 0;
+	int i;
+	for(i = 0; i < 3; i++)
+		sumAccel += abs(accel[i] - lastAccel[i]);
+	if(sumAccel >=  400) eventFlag |= EVENT_MOVED;
+	if(sumAccel >= 2000) eventFlag |= EVENT_SHAKEN;
+	memcpy(lastAccel, accel, sizeof(lastAccel));
+#if 0
+char buf[100] = {0};
+int ret = snprintf(buf,64,"%5ld %5ld %5ld %4ld %5ld %5ld ", accel[0], accel[1], accel[2], tiltX/10, tiltY/10, sumAccel);
+Serial.println(buf);
+#endif
+	uint8_t buf[8]={0};
+	buf[0] = eventFlag;
+	buf[1] = eventFlag>>8;
 	buf[2] = (digitalRead(PIN_BUTTON_A)^1) | ((digitalRead(PIN_BUTTON_B)^1)<<1);
 	buf[3] = 0;
 	buf[4] = tiltX;
 	buf[5] = tiltX>>8;
 	buf[6] = tiltY;
 	buf[7] = tiltY>>8;
-
 	sendBin(buf,8);
+
+	eventFlag = 0;
 }
