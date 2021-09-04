3bf790c8a03e23edad39c41492ff0c841e380662
sohtamei <sohta02@fa2.so-net.ne.jp>
2021-09-05 06:21:45 +0900

    - support unitCAM

---
+++ libraries/unitCam/robot_pcmode/main.h
@@ -0,0 +1,15 @@
+#ifndef main_h
+#define main_h
+
+extern WebsocketsServer wsServer;
+void _setup(const char* ver);
+void _loop(void);
+
+void _setLED(uint8_t onoff);
+
+//#define DEVICE_M5CAMERA
+//#define DEVICE_M5TIMERCAM
+//#define DEVICE_ESP32_CAM
+//#define DEVICE_ESP32CAM
+#define DEVICE_UNITCAM
+#endif  // main_h
