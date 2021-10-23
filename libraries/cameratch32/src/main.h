4827f2ccf9f8dfd2212000a31d2522c95461aa31
sohtamei <sohta02@fa2.so-net.ne.jp>
2021-10-24 01:48:14 +0900

    cameratch32追加

---
+++ libraries/cameratch32/src/main.h
@@ -0,0 +1,20 @@
+#ifndef main_h
+#define main_h
+
+extern WebsocketsServer wsServer;
+void _setup(const char* ver);
+void _loop(void);
+
+void _setLED(uint8_t onoff);
+void _setCameraMode(uint8_t mode, uint8_t gain);
+
+#define CAMERA_ENABLED
+//#define DEVICE_M5CAMERA
+//#define DEVICE_M5TIMERCAM
+//#define DEVICE_ESP32_CAM
+//#define DEVICE_ESP32CAM
+//#define DEVICE_UNITCAM
+//#define QCAI
+#define DEVICE_CAMERATCH32
+#define XCLK_FREQ 10000000
+#endif  // main_h
