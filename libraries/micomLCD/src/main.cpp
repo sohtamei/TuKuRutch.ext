f646068b10f734c9512b7ab8f0fe42b1085663be
sohtamei <sohta02@fa2.so-net.ne.jp>
2022-10-18 07:32:43 +0900

    - パネル整理、AUTO対応、他

--- libraries/micomLCD/src/main.cpp
+++ libraries/micomLCD/src/main.cpp
@@ -2,21 +2,12 @@
 #include <Arduino.h>
 #include <stdint.h>
 #include "main.h"
-#include "panel_def.h"
 
 lgfx::LGFX_Device *lcd = NULL;
 WebsocketsServer wsServer;
 Preferences preferencesLCD;
 
-const char LcdTypeStr[][16] = {
-	"DEFAULT",		// 0
-	"SSD1306",
-	"SSD1306_32",
-	"SSD1331",
-	"3248S035",
-	"GC9A01",
-};
-
+#include "panel_def.h"
 
 void _setupLCD(int lcdType, uint8_t *config_buf, int config_size)
 {
@@ -27,30 +18,75 @@ void _setupLCD(int lcdType, uint8_t *config_buf, int config_size)
 	}
 
 	switch(lcdType) {
+	case LCDTYPE_AUTO:
+	case LCDTYPE_AUTO_ROLL:
+		lcd = new LGFX();
+		lcd->init();
+		if(lcdType == LCDTYPE_AUTO_ROLL)
+			lcd->setRotation(1);
+		break;
+
 	case LCDTYPE_SSD1306:
-	case LCDTYPE_SSD1306_32:
-		lcd = new LGFX_SSD1306(lcdType, config_buf, config_size);
-		break;
-	case LCDTYPE_SSD1331:
-		lcd = new LGFX_SSD1331(lcdType, config_buf, config_size);
-		break;
-	case LCDTYPE_3248S035:
-		lcd = new LGFX_3248S035(lcdType, config_buf, config_size);
-		break;
-	case LCDTYPE_GC9A01:
-		lcd = new LGFX_GC9A01(lcdType, config_buf, config_size);
-		break;
+	case LCDTYPE_SSD1306_32:	lcd = new LGFX_SSD1306(lcdType, config_buf, config_size); break;
+	case LCDTYPE_SSD1331:		lcd = new LGFX_SSD1331(lcdType, config_buf, config_size); break;
+	case LCDTYPE_3248S035:		lcd = new LGFX_3248S035(lcdType, config_buf, config_size); break;
+	case LCDTYPE_ROUNDLCD:		lcd = new LGFX_ROUNDLCD(lcdType, config_buf, config_size); break;
+	case LCDTYPE_MSP2807:		lcd = new LGFX_MSP2807(lcdType, config_buf, config_size); break;
 	default:
 		return;
 	}
 
-	lcd->init();
 	lcd->fillScreen(TFT_BLACK);
 	lcd->setTextColor(TFT_WHITE,TFT_BLACK);
 	lcd->setFont(&fonts::lgfxJapanGothic_12);
 	lcd->setCursor(0,0);
 	lcd->println("OK");
 	lcd->setCursor(0,0);
+}
+
+int _getLcdConfig(uint8_t* buf)
+{
+	if(!lcd) {
+		return 0;
+	}
+
+	SetL16(buf+0, lcd->width());
+	SetL16(buf+2, lcd->height());
+	buf[4] = preferencesLCD.getInt("lcdType", -1);
+
+	lgfx::IBus* bus = lcd->panel()->bus();
+	lgfx::ILight* light = lcd->panel()->light();
+
+	switch(bus->busType()) {
+	default:
+		return 5;
+	case lgfx::bus_i2c: {
+		auto cfgI2c = ((lgfx::Bus_I2C*)bus)->config();
+		buf[5] = cfgI2c.pin_sda;
+		buf[6] = cfgI2c.pin_scl;
+		return 7;
+	  }
+	case lgfx::bus_spi: {
+		auto cfgSpi = ((lgfx::Bus_SPI*)bus)->config();
+		buf[5] = cfgSpi.pin_sclk;
+		buf[6] = cfgSpi.pin_mosi;
+		buf[7] = cfgSpi.pin_miso;
+		buf[8] = cfgSpi.pin_dc;
+
+		auto cfgPanel = lcd->panel()->config();
+		buf[9] = cfgPanel.pin_cs;
+		buf[10] = cfgPanel.pin_rst;
+		buf[11] = cfgPanel.pin_busy;
+
+		if(light) {
+			auto cfgPwm = ((lgfx::Light_PWM*)light)->config();
+			buf[12] = cfgPwm.pin_bl;
+		} else {
+			buf[12] = -1;
+		}
+		return 13;
+	  }
+	}
 }
 
 static void onConnect(String ip)
@@ -82,7 +118,7 @@ void _setup(const char* ver)
 		Serial.printf("type=%s\n", LcdTypeStr[lcdType]);
 
 		_setupLCD(lcdType, config, config_num);
-		lcd->println(ver);
+		if(lcd) lcd->println(ver);
 	} while(0);
 
 	initWifi(ver, false, onConnect);
--- libraries/micomLCD/src/main.h
+++ libraries/micomLCD/src/main.h
@@ -1,8 +1,11 @@
 #ifndef main_h
 #define main_h
 
+#define LGFX_AUTODETECT
 #define LGFX_USE_V1
 #include <LovyanGFX.hpp>
+#include <LGFX_AUTODETECT.hpp>
+
 #include <TukurutchEsp.h>
 #include <Preferences.h>
 
@@ -15,16 +18,11 @@ void _loop(void);
 #define NVSCONFIG_MAX  12
 
 void _setupLCD(int lcdType, uint8_t *config_buf, int config_size);
-
-enum {
-	LCDTYPE_DEFAULT = 0,
-	LCDTYPE_SSD1306 = 1,
-	LCDTYPE_SSD1306_32 = 2,
-	LCDTYPE_SSD1331 = 3,
-	LCDTYPE_3248S035 = 4,
-	LCDTYPE_GC9A01 = 5,
-};
+int _getLcdConfig(uint8_t* buf);
 
 extern lgfx::LGFX_Device *lcd;
+
+#define GetL16(a) (((a)[1]<<8) | (a)[0])
+#define SetL16(a, b) ((a)[1] = (b)>>8,  (a)[0] = ((b) & 0xFF))
 
 #endif  // main_h
