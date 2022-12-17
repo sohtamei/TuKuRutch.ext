aaba3aa0e82e8012eec9f7e9cb578d28dda9f257
sohtamei <sohta02@fa2.so-net.ne.jp>
2022-12-18 02:15:18 +0900

    - BLE 20byte分割送信対応 - BLE softI2C対応

--- libraries/microbitV1/src/combine.sh
+++ libraries/microbitV1/src/combine.sh
@@ -1,4 +1,4 @@
 DIS=Tukurutch.microbitV1.hex
-mergehex.exe -m s110_nrf51_8.0.0_softdevice.hex robot_pcmode.ino.BBCmicrobit.hex -o $DIS
+mergehex.exe -m s110_nrf51_8.0.0_softdevice.hex src.ino.BBCmicrobit.hex -o $DIS
 cp $DIS ../../../scratch3/
 sleep 3
--- libraries/microbitV2/src/combine.sh
+++ libraries/microbitV2/src/combine.sh
@@ -1,4 +1,4 @@
 DIS=Tukurutch.microbitV2.hex
-mergehex.exe -m s113_nrf52_7.2.0_softdevice.hex robot_pcmode.ino.BBCmicrobitV2.hex -o $DIS
+mergehex.exe -m s113_nrf52_7.2.0_softdevice.hex src.ino.BBCmicrobitV2.hex -o $DIS
 cp $DIS ../../../scratch3/
 sleep 3
